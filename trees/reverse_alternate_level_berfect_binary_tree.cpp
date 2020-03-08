b746629b781e776a7ef2ef9d07a470d0056b2f57
Amit <amit99kumar4826@gmail.com>
2020-03-09 01:14:10 +0530

    reverse alternate level prefect binary tree

--- trees/reverse_alternate_level_berfect_binary_tree.cpp
+++ trees/reverse_alternate_level_berfect_binary_tree.cpp
@@ -19,7 +19,45 @@ public:
 // The main function to reverse alternate nodes of a binary tree 
 void reverseAlternate(Node *root) 
 { 
- 	return;
+ 	if(root==NULL)
+        return ;
+    queue <Node *> q1;
+    queue <Node *> q2;
+    vector <Node * > v;
+    q2.push(root);
+    while((!q1.empty()) || (!q2.empty()))
+    {
+        while(!q2.empty())
+        {
+            Node *temp = q2.front();
+            q2.pop();
+            if(temp->left)
+                q1.push(temp->left);
+            if(temp->right)
+                q1.push(temp->right);
+        }
+        
+        while(!q1.empty())
+        {
+            Node *temp = q1.front();
+            v.push_back(temp);
+            q1.pop();
+            if(temp->left)
+                q2.push(temp->left);
+            if(temp->right)
+                q2.push(temp->right);
+        }
+
+        int l = v.size();
+        for(int i=0;i<l/2;i++)
+        {
+            int temp = v[i]->data;
+            v[i]->data = v[l-1-i]->data;
+            v[l-1-i]->data = temp;
+            // cout<<v[i]->data<<" ";
+        }
+        v.clear();
+    }
 } 
   
 // A utility function to print indorder traversal of a 
@@ -48,8 +86,8 @@ int main()
     root->left->right->right = new Node('k'); 
     root->right->left->left = new Node('l'); 
     root->right->left->right = new Node('m'); 
-    root->right->right->left = new Node('n'); 
-    root->right->right->right = new Node('o'); 
+    // root->right->right->left = new Node('n'); 
+    // root->right->right->right = new Node('o'); 
   
     cout << "Inorder Traversal of given tree\n"; 
     printInorder(root); 
